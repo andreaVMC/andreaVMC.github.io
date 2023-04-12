b118f7eb267605ca854d06a4f1a372fdc0b7e29f
andrea <andrea@vaccaro.it>
2023-04-12 23:36:06 +0200

    parallax section

---
+++ JS/parallax-index.js
@@ -0,0 +1,19 @@
+let conoscimi = document.querySelector(".conoscimi");
+conoscimi=conoscimi.querySelector(".frontpage");
+
+//definisco gli elementi
+var blackhole=conoscimi.querySelector("#blackhole");
+var pianeti=conoscimi.querySelector("#pianeti");
+var satelliti=conoscimi.querySelector("#satelliti");
+var testo=conoscimi.querySelector("#testo");
+var bottone=conoscimi.querySelector("#bottone");
+
+let value;
+window.addEventListener('scroll',function(){
+    value=window.scrollY;
+    blackhole.style.left = (value*0.25)+"px";
+    pianeti.style.left = -(value*0.25-200)+"px";
+    satelliti.style.left = -(value*0.25-200)+"px";
+    testo.style.top = -(value*0.25-300)+"px";
+    bottone.style.top = -(value*0.25-650)+"px";
+});
