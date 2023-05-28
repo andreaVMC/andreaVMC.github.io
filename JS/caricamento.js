8aa98fdd2aae8b9b385cb5a25b8d079ed97936e1
andrea <andrea@vaccaro.it>
2023-05-28 18:23:37 +0200

    loading changes

--- JS/caricamento.js
+++ JS/caricamento.js
@@ -1,9 +1,9 @@
 $(document).ready(function(){
     //nascondi il caricamento
     //funzione sleep
-    sleep(2500).then(() => {
-        $(".loading").fadeOut();
-        document.getElementById("caricamento").style.height = "0%";
+    sleep(4000).then(() => {
+        $("#caricamento").fadeOut();
+        
     });
 });
 
