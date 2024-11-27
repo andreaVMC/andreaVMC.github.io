9edc7cfa5745d17b3ebaeee6274f091ab996b2dc
Andrea <104673407+andreaVMC@users.noreply.github.com>
2024-11-27 21:36:39 +0100

    no shit

--- Archivio/Budget/config.php
+++ Archivio/Budget/config.php
@@ -1,6 +1,7 @@
 <?php
-    $dbhost="localhost";
-    $dbname="budget";
-    $dbuser="root";
-    $dbpassword="";
+    $dbhost="sql7.freesqldatabase.com";
+    $dbname="sql7748044";
+    $dbuser="sql7748044";
+    $dbpassword="wbJ2TM7i3r";
+    $port=3306;
 ?>
