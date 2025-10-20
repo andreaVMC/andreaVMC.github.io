64553abf9bb559e77029d2a57ebd1829d6fba8ff
Andrea Vaccaro <vaccaroandrea1227@gmail.com>
2025-10-20 21:15:35 +0200

    un po di roba

--- test/scripts/corpi.js
+++ test/scripts/corpi.js
@@ -32,6 +32,36 @@ export function anakin(n) {
     return objects; // Restituisce un array di oggetti
 }
 
+export function anakin2(n) {
+    const objects = [];
+    for (let i = 0; i < n; i++) {
+        const obj = Bodies.rectangle(
+            Math.random() * window.innerWidth,
+            Math.random() * window.innerHeight,
+            120, // Larghezza del rettangolo
+            120, // Altezza del rettangolo
+            {
+                restitution: 1,
+                frictionAir: 0.01,
+                inertia: 5000,
+                force: { 
+                    x: (Math.random() * 0.15) * (Math.random() < 0.5 ? -1 : 1),
+                    y: (Math.random() * 0.15) * (Math.random() < 0.5 ? -1 : 1)
+                },
+                render: {
+                    sprite: {
+                        texture: "img/anakin2.png", 
+                    },
+                },
+                chamfer: { radius: 15 },
+                url: "#"
+            }
+        );
+        objects.push(obj);
+    }
+    return objects; // Restituisce un array di oggetti
+}
+
 export function generatePortfolio() {
     const objects = [];
     objects.push(unipr());
